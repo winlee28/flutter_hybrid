faa0ea9729e05243854f9816ffc68d5e425655c8
李文 <liwen@ppdai.com>
2019-11-15 14:59:02 +0800

    Android&Flutter通信

--- AndroidFlutterHybrid/app/src/main/java/com/hybrid/androidflutterhybrid/MainActivity.java
+++ AndroidFlutterHybrid/app/src/main/java/com/hybrid/androidflutterhybrid/MainActivity.java
@@ -1,33 +1,203 @@
 package com.hybrid.androidflutterhybrid;
 
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import io.flutter.facade.Flutter;
 import io.flutter.facade.FlutterFragment;
+import io.flutter.plugin.common.BasicMessageChannel;
+import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodCodec;
+import io.flutter.plugin.common.StringCodec;
 import io.flutter.view.FlutterView;
 
 /**
  * Create by liwen on 2019/11/6
+ * <p>
+ * 说明：
+ * 三种Channel的调用都通过注释start-end来限定范围了
+ * 可以直接打开注释来打开相对应的方法调用，
+ * 需要配合在main.dart中打开相对应的方法
  */
 public class MainActivity extends AppCompatActivity {
+
+    String message;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         FlutterView flutterView = Flutter.createView(this, getLifecycle(), "route1");
-        setContentView(flutterView);
+        FrameLayout.LayoutParams layout =
+                new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layout.topMargin = 400;
+        addContentView(flutterView, layout);
 
 //        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 //        //container为activity_main布局中的占位符FrameLayout
 //        transaction.replace(R.id.container, Flutter.createFragment(""));
 //        transaction.commit();
 
+        TextView mTvDart = findViewById(R.id.tvDart);
+        EditText mEtTxt = findViewById(R.id.et_txt);
+        Button mBtnTitle = findViewById(R.id.btn_title);
+        message = mEtTxt.getText().toString();
+
+        /**
+         * BasicMessageChannel start
+         */
+        //初始化BasicMessageChannel
+//        BasicMessageChannel<String> basicMessageChannel =
+//                new BasicMessageChannel<>(flutterView, "BasicMessageChannelPlugin", StringCodec.INSTANCE);
+//
+//        mBtnTitle.setOnClickListener(v -> {
+//            if (!TextUtils.isEmpty(message)) {
+//                //BasicMessageChannel send
+//                basicMessageChannel.send(message, reply -> mTvDart.setText(reply));
+//
+//            } else {
+//                Toast.makeText(MainActivity.this, "在输入框中填写发送的内容", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        //basicMessageChannel receive
+//        basicMessageChannel.setMessageHandler((message, reply) -> {
+//            mTvDart.setText(message);
+//            reply.reply("收到dart数据：接受成功");
+//        });
+
+        /**
+         * basicMessageChannel end
+         */
+
+        /**
+         * MethodChannel start
+         */
+
+
+        //初始化MethodChannel
+//        MethodChannel methodChannel = new MethodChannel(flutterView, "MethodChannelPlugin");
+//
+//        mBtnTitle.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //调用dart端getPlatform方法
+//                methodChannel.invokeMethod("getPlatform", null, new MethodChannel.Result() {
+//                    @Override
+//                    public void success(@Nullable Object result) {
+//                        mTvDart.setText(result.toString());
+//                    }
+//
+//                    @Override
+//                    public void error(String errorCode, @Nullable String errorMessage, @Nullable Object errorDetails) {
+//                        mTvDart.setText(errorCode + "==" + errorMessage);
+//                    }
+//
+//                    @Override
+//                    public void notImplemented() {
+//                        mTvDart.setText("未实现getPlatform方法");
+//                    }
+//                });
+//            }
+//        });
+//
+//        //接受dart的调用
+//        methodChannel.setMethodCallHandler((call, result) -> {
+//            switch (call.method) {
+//                case "getBatteryLevel":
+//                    int batteryLevel = getBatteryLevel();
+//                    if (batteryLevel != -1) {
+//                        result.success("电量为：" + batteryLevel);
+//                    } else {
+//                        result.error("1001", "调用错误", null);
+//                    }
+//                    break;
+//                default:
+//                    result.notImplemented();
+//                    break;
+//            }
+//        });
+        /**
+         * MethodChannel end
+         */
+
+
+        /**
+         * EventChannel start
+         */
+
+        EventChannel eventChannel = new EventChannel(flutterView, "EventChannelPlugin");
+        eventChannel.setStreamHandler(new EventChannel.StreamHandler() {
+            @Override
+            public void onListen(Object arguments, EventChannel.EventSink events) {
+                events.success("电量：" + getBatteryLevel());
+                //events.error("111","出现错误","");
+                //events.endOfStream();
+            }
+
+            @Override
+            public void onCancel(Object arguments) {
+            }
+        });
+
+        /**
+         * EventChannel end
+         */
+
+
+        mEtTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                message = s.toString();
+            }
+        });
+
     }
+
+    private int getBatteryLevel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            return batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            Intent intent = new ContextWrapper(getApplicationContext()).
+                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            return (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
+    }
+
 }
 
 
--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
+import 'package:flutter/services.dart';
+import 'dart:io';
+
+/// 说明：
+/// 三种channel的调用已经封装在了在_onPressed和initState方法中，可以通过打开相对应的注释来打开相关方法调用
+/// 需要配合在MainActivity中打开相对应的方法配合调用
 void main() => runApp(MyApp(
       //通过window.defaultRouteName获取从native传递过来的参数
       initParams: window.defaultRouteName,
@@ -33,13 +41,198 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = new TextEditingController();
+
+  String showMessage = "";
+  String response = "";
+  int batteryLevel;
+  StreamSubscription streamSubscription;
+
+  ///BasicMessageChannel start
+
+  // init
+  static const BasicMessageChannel<String> _basicMessageChannel =
+      BasicMessageChannel("BasicMessageChannelPlugin", StringCodec());
+
+  // receive
+  void handleBasicMessageChannelReceive() {
+    _basicMessageChannel
+        .setMessageHandler((String message) => Future<String>(() {
+              setState(() {
+                showMessage = message;
+              });
+              return "收到Native的消息：接受成功";
+            }));
+  }
+
+  //send
+  void handleBasicMessageChannelSend() async {
+    response = await _basicMessageChannel.send(_controller.text);
+    setState(() {
+      showMessage = response;
+    });
+  }
+
+  ///BasicMessageChannel end
+
+  ///MethodChannel start
+
+  // init
+  static const MethodChannel _methodChannel =
+      MethodChannel("MethodChannelPlugin");
+
+  // receive
+  void handleMethodChannelReceive() {
+    Future<dynamic> platformCallHandler(MethodCall call) async {
+      switch (call.method) {
+        case "getPlatform":
+          return getPlatformName(); //调用success方法
+//          return PlatformException(code: '1002',message: "出现异常"); //调用error
+          break;
+      }
+    }
+
+    _methodChannel.setMethodCallHandler(platformCallHandler);
+//    _methodChannel.setMethodCallHandler(null); //调用notImplemented
+  }
+
+  // send
+  void handleMethodChannelSend() async {
+    try {
+      response = await _methodChannel.invokeMethod("getBatteryLevel");
+      print(response);
+      setState(() {
+        showMessage = response;
+      });
+    } catch (e) {
+      //捕获error和notImplemented异常
+      setState(() {
+        showMessage = e.message;
+      });
+    }
+  }
+
+  ///MethodChannel end
+
+  ///EventChannel start
+
+  //init
+  static const EventChannel _eventChannel = EventChannel("EventChannelPlugin");
+
+  //receive
+  void handleEventChannelReceive() {
+    streamSubscription = _eventChannel
+        .receiveBroadcastStream()//可以携带参数
+        .listen(_onData, onError: _onError, onDone: _onDone);
+  }
+
+  void _onDone() {
+    setState(() {
+      showMessage = "endOfStream";
+    });
+  }
+
+  _onError(error) {
+    setState(() {
+      PlatformException platformException = error;
+      showMessage = platformException.message;
+    });
+  }
+
+  void _onData(message) {
+    setState(() {
+      showMessage = message;
+    });
+  }
+
+  ///EventChannel end
+
+  void _onPressed() async {
+    ///BasicMessageChannel send
+    //handleBasicMessageChannelSend();
+
+    ///MethodChannel send
+    //handleMethodChannelSend();
+  }
+
+  @override
+  void initState() {
+    ///BasicMessageChannel receive
+    //handleBasicMessageChannelReceive();
+
+    ///MethodChannel receive
+    //handleMethodChannelReceive();
+
+    ///MethodChannel receive
+    handleEventChannelReceive();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (streamSubscription != null) {
+      streamSubscription.cancel();
+      streamSubscription = null;
+    }
+
+    super.dispose();
+  }
+
+  String getPlatformName() {
+    if (Platform.isIOS) {
+      return "iOS platform";
+    } else if (Platform.isAndroid) {
+      return "Android platform";
+    }
+    return "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _widgetRoute(widget.initParams),
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+            child: TextField(
+              controller: _controller,
+              decoration: new InputDecoration(
+                hintText: '传递给Native的数据',
+              ),
+            ),
+          ),
+          RaisedButton(
+            onPressed: _onPressed,
+            child: Text("Msg To Native"),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: Row(
+//              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "来自native的消息：",
+                  style: TextStyle(color: Colors.black, fontSize: 15),
+                ),
+                Text(
+                  showMessage,
+                  style: TextStyle(color: Colors.red, fontSize: 15),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
-    );
+    )
+
+        //集成时打开
+//      Center(
+//        child: _widgetRoute(widget.initParams),
+//      ),
+        );
   }
 }
 
